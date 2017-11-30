d3f27067c3c90d979fb3feb7d034bae55ecebdab
fj2016 <kfigiel.projekty@gmail.com>
2017-11-30 21:56:37 +0100

    add new book

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -15,11 +15,19 @@ export class BooksListComponent implements OnInit {
 
   ngOnInit() {
     this.booksObservable = this.getBooks('/Books');
+    this.addBook('/Books');
     console.log(this.booksObservable);
   }
 
   getBooks(booksPath): Observable<any> {
     return this.db.list(booksPath).valueChanges();
+  }
+
+  addBook(booksPath) {
+    this.db.database.ref(booksPath + '/Angular').set({
+      title: 'Angular - the complete guide',
+      description: 'The complete guide of an Angular framework',
+    });
   }
 
 }
