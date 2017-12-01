bf0fc484662a1f14982d6a094fb94459b370d96c
kfigiel <k.figiel@mobilinktech.pl>
2017-12-01 08:21:10 +0100

    login modal fixes

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -17,16 +17,6 @@ export class AppNavbarComponent implements OnInit {
     const modalRef = this.modalService.open(LoginModalComponent, { size: 'lg' });
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
-
   ngOnInit() {
   }
 
